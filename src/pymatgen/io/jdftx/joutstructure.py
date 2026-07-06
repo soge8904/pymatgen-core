1cbe08d0de1ec73bad550eef5427ecf5251f16be
Sophie Gerits <soge8904@colorado.edu>
2026-07-06 10:17:41 -0600

    The selective dynamics always returned 1. Now will return 0 if fixed, 1 if not fixed

--- src/pymatgen/io/jdftx/joutstructure.py
+++ src/pymatgen/io/jdftx/joutstructure.py
@@ -1090,7 +1090,8 @@ def _parse_posn_line(
         velocity = np.array([float(x) for x in psplit[6:9]])
     # Convert the movescale tag to a redundant selective_dynamics tag to match the expected shape
     # (int(bool(v)) used since technically something like "0.1" can be passed to JDFTx to indicate non-freezing)
-    sd = [int(bool(posn_line.split()[offset + 5])) for _ in range(3)]
+    # sd = [int(bool(posn_line.split()[offset + 5])) for _ in range(3)]
+    sd = [int(bool(float(psplit[offset + 5]))) for _ in range(3)]
     # Only trigger the try/except block if we have enough elements in the line
     if len(psplit) > offset + 6:
         # Check for constraints (protected by try/except since its genuinely more likely that an accidental edit
